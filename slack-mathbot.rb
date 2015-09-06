ea7c5ac88e72efa39a4bb62ca6a57eb2a1ccd85a
Abelardo J. Marquez <ajmarquezg@gmail.com>
2015-09-06 02:23:11 -0430

    Added the reaction.list method

--- slack-mathbot.rb
+++ slack-mathbot.rb
@@ -16,10 +16,32 @@ client = Slack::RealTime::Client.new
 
 client.on :hello do
   puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
+
 end
 
 client.on :message do |data|
   puts data
+
+  #  Para poder acceder a la API web es necesarion colocar 'web_client' despues de declarar el cliente,
+  #  por ejemplo:
+  #
+  #  client.web_client.reactions_list
+  #
+  #  Este metodo en particular se encarga de recoger la lista de reacciones utilizadas en el chat y devuelve un JSON
+  #  que contiene las reaciones el mensaje y demás. Para tomar las reacciones de un usuario en particular se agrega
+  #  user: data['user']
+  #
+  #  Al realizar client.web_client.reactions_list user: data['user'] se obtiene a lista del usuario en particular
+  #  que escribió el último mensaje.
+  #
+  #  TO-DO:
+  #  1.- Parsear por el JSON y reconocer las reacciones determinadas.
+  #  2.- Con algun comando recoger las reacciones a trackear. Esto puede ser al hablar con el bot
+  #  3.- El nombre de mathBot a Yodabot cambiar se debe. Una imagen correspondiente, debe agregarse.
+  #
+  #   
+
+  puts client.web_client.reactions_list user: data['user']
 
   client.typing channel: data['channel']
 
@@ -30,6 +52,8 @@ client.on :message do |data|
     client.message channel: data['channel'], text: "Say Hi, and tell her to stop playing games!"
   end
 end
+
+
 
 
 client.start!
